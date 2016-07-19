e0d227480cf05309a7defe5970cc796329f98f6a
unknown <ecoz@sina.cn>
2016-07-19 09:56:10 +0800

    edit browserExcecute

--- src/main/java/topdeep/autotest/biz/executer/impl/browser/HttpGetAction.java
+++ src/main/java/topdeep/autotest/biz/executer/impl/browser/HttpGetAction.java
@@ -8,15 +8,14 @@ import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.openqa.selenium.WebDriver;
-import org.springframework.stereotype.Service;
 
 import topdeep.autotest.biz.executer.impl.BaseExecute;
 import topdeep.autotest.entity.constant.EnumType.ObjType;
 import topdeep.autotest.entity.constant.EnumType.ParamType;
 import topdeep.autotest.entity.constant.EnumType.TestContextDataKey;
 import topdeep.autotest.entity.constant.EnumType.TestResult;
-import topdeep.autotest.entity.db.AtObjParams;
-import topdeep.autotest.entity.db.AtUserCaseAction;
+import topdeep.autotest.entity.data.AtObjParams;
+import topdeep.autotest.entity.data.AtUserCaseAction;
 import topdeep.autotest.entity.execute.ObjRegisterAttribute;
 import topdeep.autotest.entity.execute.UserCaseActionExecute;
 import topdeep.autotest.entity.execute.UserCaseExecute;
@@ -25,27 +24,10 @@ import topdeep.autotest.entity.execute.UserCaseExecute;
  * @author niexin
  *
  */
-public class HttpGetAction extends BaseExecute implements UserCaseActionExecute {
+public class HttpGetAction  implements UserCaseActionExecute {
 
 	public static String PARAM_TARGET_URL = "targetUrl";
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see topdeep.autotest.entity.execute.BaseExecute#getId()
-	 */
-	public String getId() {
-		return "S1C0q0_SPqOWKXQ0v1y-Q..u";
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see topdeep.autotest.entity.execute.BaseExecute#getName()
-	 */
-	public String getName() {
-		return "打开网页";
-	}
 
 	/*
 	 * (non-Javadoc)
@@ -57,12 +39,7 @@ public class HttpGetAction extends BaseExecute implements UserCaseActionExecute 
 	}
 
 	public String getActionDesc(List<AtObjParams> paramList) {
-		Map<String, AtObjParams> paramMap = convertParamListToMap(paramList);
-		String ret = getName();
-		if (paramMap.containsKey(PARAM_TARGET_URL)) {
-			ret += ": " + paramMap.get(PARAM_TARGET_URL).getParamValue();
-		}
-		return ret;
+		return null;
 	}
 
 	/*
@@ -73,11 +50,11 @@ public class HttpGetAction extends BaseExecute implements UserCaseActionExecute 
 	 */
 	public TestResult execute(UserCaseExecute userCaseExecute, AtUserCaseAction userCaseAction, List<AtObjParams> paramList, Map<String, Object> data,
 			Log taskLog) throws Exception {
-		Map<String, AtObjParams> paramMap = convertParamListToMap(paramList);
+		Map<String, AtObjParams> paramMap ;
 		BrowserUserCaseExecute execute = (BrowserUserCaseExecute) userCaseExecute;
 		if (paramMap.containsKey(PARAM_TARGET_URL) && data.containsKey(TestContextDataKey.Driver.getValue())) {
 			WebDriver wd = (WebDriver) data.get(TestContextDataKey.Driver.getValue());
-			String url = (String) paramMap.get(PARAM_TARGET_URL).getParamValue();
+			String url = ;
 			taskLog.debug("open url: " + url);
 			execute.httpGet(wd, url);
 			return TestResult.Success;
@@ -85,19 +62,25 @@ public class HttpGetAction extends BaseExecute implements UserCaseActionExecute 
 		return TestResult.Fail;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see topdeep.autotest.biz.executer.impl.BaseExecute#getParamList()
-	 */
-	@Override
-	public List<AtObjParams> getInitParamList() {
-		List<AtObjParams> paramList = super.getInitParamList();
-		paramList.add(createNewParam(getId(), ObjType.UserCaseActionExecute.getValue(), getId(), PARAM_TARGET_URL, "目标地址", "需要打开的URL完整地址",
-				ParamType.String.getValue(), "", null, null, "1", "1", 1));
-		return paramList;
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<AtObjParams> getInitParamList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getBeanVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	
 }
